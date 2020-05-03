9b78e01cfd6c8ad956b215b124dc032ef430d1dd
Fabio Priori <fabio.priori2@gmail.com>
2020-05-03 18:34:43 +0200

    code refactor

--- index.php
+++ index.php
@@ -17,97 +17,7 @@
   </head>
   <body>
     
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div>
-        <a class="navbar-brand" href="#">
-          <img src="img/logo.png" width="" height="40" alt="">
-        </a>
-        <span class="navbar-brand mb-0 h1">Twitty</span>
-      </div>
-      
-      <div class="mx-auto">
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-
-      <!-- Show only when NOT logged in -->
-      <button type="button" class="btn btn-primary btn-light" data-toggle="modal" data-target=".bd-sign-up-modal">Sign up</button>
-      <div class="modal fade bd-sign-up-modal" tabindex="-1" role="dialog" aria-labelledby="signUpDialog" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle"lastName>Sign up</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <form>
-                <div class="form-group">
-                  <label for="name">Name</label>
-                  <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter Name">
-                </div>
-                <div class="form-group">
-                  <label for="email">Last Name</label>
-                  <input type="text" class="form-control" id="lastName" aria-describedby="emailHelp" placeholder="Enter Last Name">
-                </div>
-                <div class="form-group">
-                  <label for="email">Email address</label>
-                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" class="form-control" id="password" placeholder="Password">
-                </div>
-                <button type="submit" class="btn btn-primary">Sign up</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Show only when NOT logged in -->
-      <button type="button" class="btn btn-primary btn-light" data-toggle="modal" data-target=".bd-log-in-modal">Log in</button>
-      <div class="modal fade bd-log-in-modal" tabindex="-1" role="dialog" aria-labelledby="logInDialog" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle"lastName>Log in</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <form>
-                <div class="form-group">
-                  <label for="email">Email address</label>
-                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" class="form-control" id="password" placeholder="Password">
-                </div>
-                <button type="submit" class="btn btn-primary">Log in</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <!-- Show only when logged in -->
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle btn-light" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Fabio Priori
-        </button>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2">
-          <button class="dropdown-item" type="button">Change details</button>
-          <button class="dropdown-item" type="button">Change password</button>
-          <button class="dropdown-item" type="button">Log out</button>
-        </div>
-      </div>
-    </nav>
+    <?php include 'navbar.php'; ?>
 
     <div class="container">
       <div class="row">
@@ -123,40 +33,7 @@
             </div>
           </div>
           
-          <?php
-            $conn=mysqli_connect("localhost","root","","db_twitty");
-            $query="SELECT u.nome, u.cognome, t.data_creazione, t.testo
-                    FROM `tweet` t
-                    JOIN `utenti` u
-                    ON t.id_utente = u.id
-                    ORDER BY t.data_creazione DESC";
-            $r=mysqli_query($conn,$query);
-            while ($row= mysqli_fetch_array($r)):
-          ?>
-
-          <div class="tweet">
-            <h4><?php echo $row[0] . ' ' . $row[1]; ?></h4>
-            <h6><?php echo $row[2]?></h6>
-            <p><?php echo $row[3]?></p>
-            <div class="d-flex justify-content-between">
-              <div>
-                <img src="img/like.png" width="" height="25" alt="">
-                <span>23</span>
-              </div>
-
-              <div class="btn-group">
-                <button class="btn btn-secondary btn-sm dropdown-toggle btn-light" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Options
-                </button>
-                <div class="dropdown-menu dropdown-menu-right">
-                  <a class="dropdown-item" href="#">Edit</a>
-                  <a class="dropdown-item" href="#">Delete</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <?php endwhile; ?>
+          <?php include 'board.php'; ?>
 
         </div>
 
