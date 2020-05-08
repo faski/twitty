7d39201a90425c3eb75cbf33e56eb1eb698824ce
Fabio Priori <fabio.priori2@gmail.com>
2020-05-08 13:38:47 +0200

    Implemented delete tweet

--- board.php
+++ board.php
@@ -1,6 +1,6 @@
 <?php
 $conn=mysqli_connect("localhost","root","","db_twitty");
-$query="SELECT u.nome, u.cognome, t.data_creazione, t.testo
+$query="SELECT u.nome, u.cognome, t.data_creazione, t.testo, t.id_utente, t.id
         FROM `tweet` t
         JOIN `utenti` u
         ON t.id_utente = u.id
@@ -10,25 +10,36 @@ while ($row= mysqli_fetch_array($r)):
 ?>
 
 <div class="tweet">
-<h4><?php echo $row[0] . ' ' . $row[1]; ?></h4>
-<h6><?php echo $row[2]?></h6>
-<p><?php echo $row[3]?></p>
-<div class="d-flex justify-content-between">
+  <h4><?php echo $row[0] . ' ' . $row[1]; ?></h4>
+  <h6><?php echo $row[2]?></h6>
+  <p><?php echo $row[3]?></p>
+  <div class="d-flex justify-content-between">
     <div>
-    <img src="img/like.png" width="" height="25" alt="">
-    <span>23</span>
+      <img src="img/like.png" width="" height="25" alt="">
+      <span>23</span>
     </div>
 
-    <div class="btn-group">
-    <button class="btn btn-secondary btn-sm dropdown-toggle btn-light" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Options
-    </button>
-    <div class="dropdown-menu dropdown-menu-right">
-        <a class="dropdown-item" href="#">Edit</a>
-        <a class="dropdown-item" href="#">Delete</a>
-    </div>
-    </div>
-</div>
+    <?php
+      if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION['user_id'] == $row[4]): ?>
+
+      <div class="btn-group">
+        <button class="btn btn-secondary btn-sm dropdown-toggle btn-light" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Options
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+          <form action="" method="post">
+            <button type="submit" class="dropdown-item">Edit</button>
+          </form>
+          <form action="src/tweet/delete_tweet.php" method="post">
+            <input id="deleteTweetId" name="tweetId" type="hidden" value="<?php echo $row[5]?>">
+            <button type="submit" class="dropdown-item">Delete</button>
+          </form>
+        </div>
+      </div>
+
+    <?php endif; ?>
+    
+  </div>
 </div>
 
 <?php endwhile; ?>
---
+++ src/tweet/delete_tweet.php
@@ -0,0 +1,29 @@
+<?php
+  session_start();
+  $id=$_POST['tweetId'];
+
+  $conn=mysqli_connect("localhost","root","","db_twitty");
+
+  if (!$conn) {
+    echo 'Errore durante la connessione al server MySQL';
+  exit();
+  }
+  else {
+    echo "Connessione effettuata con successo";
+  }
+
+  $query="DELETE FROM tweet WHERE id = $id";
+  $r=mysqli_query($conn,$query);
+
+  if(!$r)
+    echo"query non eseguita correttamente";
+  else {
+    echo"ORDINE EFFETTUATO Grazie per l'acquisto ";
+    header("location: http://localhost/twitty/index.php");
+  }
+    
+
+  mysqli_close($conn);
+
+
+?>
